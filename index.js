7270cc68de3a6dc569d4e784a17d708cc8b5659d
Manas Behera <60974245+manaskumarbehera@users.noreply.github.com>
2021-05-20 22:32:13 +0200

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,13 @@ const PORT = process.env.PORT || 5000
 
 var app = express();
 
-app.use('/survey', surveyRouter);
+app.use('/survey', surveyRouter,function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
 app.listen(PORT, () => {
   console.log("Server running on port 5000");
  });
