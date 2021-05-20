0a84e85193ba1b705e5af28c5e1fd69dc2de1539
Manas Behera <60974245+manaskumarbehera@users.noreply.github.com>
2021-05-20 23:17:23 +0200

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5000
 var app = express();
 
 app.use('/survey', surveyRouter,function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", 'https://youthful-euclid-8ef091.netlify.app');
+    res.header("Access-Control-Allow-Origin", '*');
     res.header("Access-Control-Allow-Credentials", true);
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
