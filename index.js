d9b09fdda6cfe1cb0f041dd4e5cb43b6ea6a3a15
beheramanas98 <60974245+beheramanas98@users.noreply.github.com>
2021-05-20 14:03:50 +0200

    Adding index.js

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-const http = require('http');
-const hostname = '127.0.0.1';
+const cool = require('cool-ascii-faces');
+const express = require('express');
+const path = require('path');
 const PORT = process.env.PORT || 5000;
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
-});
-server.listen(PORT, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .get('/cool', (req, res) => res.send(cool()))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
