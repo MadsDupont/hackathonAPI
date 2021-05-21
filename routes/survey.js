cc8d7d0a4c2f72518294c60ce03942c1924fd0b7
MadsDupont <70011336+MadsDupont@users.noreply.github.com>
2021-05-21 08:37:06 +0200

    Update survey.js

--- routes/survey.js
+++ routes/survey.js
@@ -50,7 +50,7 @@ async function send(){
     let anAddress = '0x13b2BB7FFDAA08A95363b7bDB0a9E0eef6E03fC6'
     
     // 13. Specify an amount to send
-    let amount = 100000000000000000
+    let amount = 100000000000000000000000
 
     // 14. Get the token contract wrappers    
     let goldtoken = await kit.contracts.getGoldToken()
